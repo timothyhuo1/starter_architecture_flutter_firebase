d2e7250f2e8e90b73b441f92206af798e46a53fa
Timothy Huo <61216890+timothyhuo1@users.noreply.github.com>
2023-11-03 11:53:05 -1000

    job freezed

--- lib/src/features/jobs/domain/job.dart
+++ lib/src/features/jobs/domain/job.dart
@@ -1,20 +1,27 @@
-import 'package:equatable/equatable.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
+
+part 'job.freezed.dart';
+part 'job.g.dart';
 
 typedef JobID = String;
 
-@immutable
-class Job extends Equatable {
-  const Job({required this.id, required this.name, required this.ratePerHour});
-  final JobID id;
-  final String name;
-  final int ratePerHour;
+@freezed
+class Job with _$Job {
+  const factory Job({
+    required JobID id,
+    required String name,
+    required int ratePerHour,
+  }) = _Job;
+  factory Job.fromJson(Map<String, dynamic> json) =>
+      _$JobFromJson(json);
 
-  @override
-  List<Object> get props => [name, ratePerHour];
-
-  @override
-  bool get stringify => true;
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'ratePerHour': ratePerHour,
+    };
+  }
 
   factory Job.fromMap(Map<String, dynamic> data, String id) {
     final name = data['name'] as String;
@@ -24,12 +31,5 @@ class Job extends Equatable {
       name: name,
       ratePerHour: ratePerHour,
     );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'name': name,
-      'ratePerHour': ratePerHour,
-    };
   }
 }
